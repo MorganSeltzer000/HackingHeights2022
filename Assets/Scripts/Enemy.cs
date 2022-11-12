73d20eee0955f3e88e58df6ba5801e042574c83b
CRUVOD <haoran0088@hotmail.com>
2022-11-12 16:35:46 -0500

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,12 +7,37 @@ public class Enemy : MonoBehaviour
     public List<EnemyMove> movementBehaviour;
 
     public float speed;
+    public bool loopMovementBehaviour;
 
-    int moveIndex = 0;
+    Vector2 movementDirection;
 
-    private IEnumerator MoveIndexIncrease()
+    private void Start()
     {
-        moveIndex += 1;
+        movementDirection = Vector2.zero;
+        StartCoroutine(MovementControl());
+    }
 
+    private void Update()
+    {
+        Move();
+    }
+
+    private IEnumerator MovementControl()
+    {
+        foreach (EnemyMove enemyMove in movementBehaviour)
+        {
+            movementDirection = enemyMove.direction;
+            yield return new WaitForSeconds(enemyMove.duration);
+        }
+
+        if (loopMovementBehaviour)
+        {
+            StartCoroutine(MovementControl());
+        }
+    }
+
+    private void Move()
+    {
+        this.transform.position += (Vector3) movementDirection * speed * Time.deltaTime;
     }
 }
