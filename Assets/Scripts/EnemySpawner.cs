e152338a69db6c28e24322f67852e10560d53c5b
CRUVOD <haoran0088@hotmail.com>
2022-11-12 18:08:22 -0500

    Spawner Beginnings

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,20 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemySpawner : MonoBehaviour
 {
     public float[] xRange = new float[2];
     public float[] yRange = new float[2];
 
+    public List<Enemy> enemyPrefabs;
+
+    public int enemiesPerWaveMax;
+    public float timeBetweenWaves;
+
+    int numOfEnemyTypes;
+
+    public bool SpawnerEnabled;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        numOfEnemyTypes = enemyPrefabs.Count;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        while (SpawnerEnabled)
+        {
+            SpawnRandom();
+            yield return new WaitForSeconds(timeBetweenWaves);
+        }
+    }
+
+    private void SpawnRandom()
+    {
+        int enemiesToSpawn = Random.Range(0, enemiesPerWaveMax + 1);
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+
+        }
+
     }
 }
